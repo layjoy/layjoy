be4b5996c67c7525d7ecd880f611cb3eea689625
layjoy <layjoy@users.noreply.github.com>
2026-04-20 06:58:05 +0800

    优化构建速度 - 添加 Gradle 缓存、并行构建和阿里云镜像

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,25 @@
 // Top-level build file
+buildscript {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
+}
+
+allprojects {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        google()
+        mavenCentral()
+    }
 }
